07cf7cd8dd1e5be60902a55cdead3e71fed9d16f
Jim Kelley <kelljim0414@yahoo.com>
2014-05-16 16:48:50 -0400

    Changed version to alpha-7

--- src/edu/rutgers/MOST/presentation/GraphicalInterfaceConstants.java
+++ src/edu/rutgers/MOST/presentation/GraphicalInterfaceConstants.java
@@ -337,7 +337,7 @@ public class GraphicalInterfaceConstants {
     // about
     public static final String ABOUT_BOX_TITLE = "About MOST";
     public static final String ABOUT_BOX_TEXT = "MOST - Metabolic Optimization and Simulation Tool.";	
-    public static final String ABOUT_BOX_VERSION_TEXT = "Version: alpha-6";
+    public static final String ABOUT_BOX_VERSION_TEXT = "Version: alpha-7";
     
     public static final String ABOUT_LICENSE_URL = "http://most.ccib.rutgers.edu/help.html#about";
     
