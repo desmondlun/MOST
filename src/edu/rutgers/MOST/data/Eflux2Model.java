c463ec08607e8344fca29715af5c9d6d72f0453c
Tony <CodeGuro@gmail.com>
2014-06-22 14:24:53 -0400

    Decoupled ModelParser from Eflux-2
    ModelParser can be easily used in other algorithms such as Eflux-2 and SPOT

--- src/edu/rutgers/MOST/data/Eflux2Model.java
+++ src/edu/rutgers/MOST/data/Eflux2Model.java
@@ -1,193 +1,26 @@
 package edu.rutgers.MOST.data;
 
 import java.util.Map;
-import java.util.Vector;
 
 public class Eflux2Model extends FBAModel
 {
 	public void formatFluxBoundsfromTransciptomicData( final Map< String, Double > data )
 	{
-		class Interpreter
-		{
-			class Lexicon
-			{
-				String string;
-				int idx = 0;
-				
-				String word = "";
-				
-				public Lexicon( String string )
-				{
-					this.string = string;
-					advance();
-				}
-				void skip()
-				{
-					while( idx < string.length()
-							&& Character.isWhitespace( string.charAt( idx ) ) )
-						++idx;
-				}
-				String advance()
-				{
-					skip();
-					word = "";
-					if( idx < string.length() )
-						switch( string.charAt( idx ) )
-						{
-						case '(':
-						case ')':
-						case '-':
-							word += string.charAt( idx++ );
-							return word;
-						case '_':
-							word += string.charAt( idx++ );
-							while( Character.isAlphabetic( string.charAt( idx ) ) )
-								word += string.charAt( idx++ );
-							word += string.charAt( idx++ );
-							if( word.equals( "_OR_" ) )
-								word = "or";
-							else if( word.equals( "_AND_" ) )
-								word = "and";
-							break;
-						default:
-							while( idx < string.length() &&
-									( Character.isAlphabetic( string.charAt( idx ) )
-									|| Character.isDigit( string.charAt( idx ) )
-									|| string.charAt( idx ) == '.' )
-								)
-							{
-								word += string.charAt( idx++ );
-							}
-						}
-					return word;
-				}
-				String getToken()
-				{
-					return word;
-				}
-			}
-			Lexicon lexer;
-			Vector< String > byteCode = new Vector< String >();
-			Vector< Double > values = new Vector< Double >();
-			Vector< Double > stack = new Vector< Double >();
-			
-			public Interpreter( String expression ) throws Exception
-			{
-				lexer = new Lexicon( expression );
-				parseExpression();
-			}
-			public Double getValue() throws Exception
-			{
-				while( !byteCode.isEmpty() )
-				{
-					switch( byteCode.firstElement() )
-					{
-					case "push":
-						stack.add( values.firstElement() );
-						values.remove( 0 );
-						break;
-					case "and":
-						stack.set( stack.size() - 2, Math.min( stack.get( stack.size() - 2 ), stack.get( stack.size() - 1 ) ) );
-						stack.remove( stack.size() - 1 );
-						break;
-					case "or":
-						stack.set( stack.size() - 2, stack.get( stack.size() - 2 ) +  stack.get( stack.size() - 1 ) );
-						stack.remove( stack.size() - 1 );
-						break;
-					case "negate":
-						stack.set( stack.size() - 1, -stack.get( stack.size() - 1 ) );
-						break;
-						default:
-							throw new Exception( "Unsupportd operation \""
-									+ byteCode.firstElement() + " \"" );
-					}
-					byteCode.remove( 0 );
-				}
-				if( stack.size() != 1 )
-					throw new Exception( "Interpretor getValue() error - stack size" );
-				return stack.firstElement();
-			}
-			
-			private void parseExpression() throws Exception
-			{
-				parseOr();
-			}
-			private void parseOr() throws Exception
-			{
-				parseAnd();
-				while( lexer.getToken().toLowerCase().equals( "or" ) )
-				{
-					String token = lexer.getToken().toLowerCase();
-					lexer.advance();
-					parseAnd();
-					byteCode.add( token );
-				}
-			}
-			private void parseAnd() throws Exception
-			{
-				parsePrefix();
-				while( lexer.getToken().toLowerCase().equals( "and" ) )
-				{
-					String token = lexer.getToken().toLowerCase();
-					lexer.advance();
-					parsePrefix();
-					byteCode.add( token );
-				}
-			}
-			private void parsePrefix() throws Exception
-			{
-				Vector< String > codeAppend = new Vector< String >();
-				while( lexer.getToken().equals( "-" ) )
-				{
-					codeAppend.add( "negate" );
-					lexer.advance();
-				}
-				parsePostfix();
-				for( String code : codeAppend )
-					byteCode.add( code );
-			}
-			private void parsePostfix() throws Exception
-			{
-				parseClause();
-				//no postfix necessary
-			}
-			private void parseClause() throws Exception
-			{
-				if( lexer.getToken().equals( "(" ) )
-					parseParenthesis();
-				else
-				{
-					if( data.containsKey( lexer.getToken() ) )
-						values.add( data.get( lexer.getToken() ) );
-					else
-					{
-						System.out.println( "Gene \"" + lexer.getToken() + "\" not in CSV database, replacing val with infinity" );
-						values.add( Double.POSITIVE_INFINITY );
-					}
-					
-					byteCode.add( "push" );
-					lexer.advance();
-				}
-			}
-			private void parseParenthesis() throws Exception
-			{
-				if( !lexer.getToken().equals( "(" ) )
-					throw new Exception( "\"(\" expected" );
-				lexer.advance();
-				parseExpression();
-				if( !lexer.getToken().equals( ")" ) )
-					throw new Exception( "Parser error: missing \")\"" );
-				lexer.advance();
-			}
-			
-		}
 		try
 		{
 			for( SBMLReaction reaction : reactions )
 			{
 				try
 				{
-					Interpreter parser = new Interpreter( reaction.getGeneAssociation() );
+					ModelParser parser = new ModelParser( reaction.getGeneAssociation(), data )
+					{
+						@Override
+						protected Double substitute( String token )
+						{
+							System.out.println( "Gene \"" + token + "\" not in database, replacing val with infinity" );
+							return Double.POSITIVE_INFINITY;
+						}
+					};
 					double fluxBound = parser.getValue();
 					reaction.setLowerBound( reaction.getLowerBound() >= 0.0 ? 0.0 : -fluxBound );
 					reaction.setUpperBound( reaction.getUpperBound() < 0.0 ? 0.0 : fluxBound  );
---
+++ src/edu/rutgers/MOST/data/ModelParser.java
@@ -0,0 +1,184 @@
+package edu.rutgers.MOST.data;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+class ModelParser
+{
+	private class Lexicon
+	{
+		String string;
+		int idx = 0;
+		
+		String word = "";
+		
+		public Lexicon( String string )
+		{
+			this.string = string;
+			advance();
+		}
+		void skip()
+		{
+			while( idx < string.length()
+					&& Character.isWhitespace( string.charAt( idx ) ) )
+				++idx;
+		}
+		String advance()
+		{
+			skip();
+			word = "";
+			if( idx < string.length() )
+				switch( string.charAt( idx ) )
+				{
+				case '(':
+				case ')':
+				case '-':
+					word += string.charAt( idx++ );
+					return word;
+				case '_':
+					word += string.charAt( idx++ );
+					while( Character.isAlphabetic( string.charAt( idx ) ) )
+						word += string.charAt( idx++ );
+					word += string.charAt( idx++ );
+					if( word.equals( "_OR_" ) )
+						word = "or";
+					else if( word.equals( "_AND_" ) )
+						word = "and";
+					break;
+				default:
+					while( idx < string.length() &&
+							( Character.isAlphabetic( string.charAt( idx ) )
+							|| Character.isDigit( string.charAt( idx ) )
+							|| string.charAt( idx ) == '.' )
+						)
+					{
+						word += string.charAt( idx++ );
+					}
+				}
+			return word;
+		}
+		String getToken()
+		{
+			return word;
+		}
+	}
+	Lexicon lexer;
+	Vector< String > byteCode = new Vector< String >();
+	Map< String, Double > data = new HashMap< String, Double >();
+	Vector< Double > values = new Vector< Double >();
+	Vector< Double > stack = new Vector< Double >();
+	
+	public ModelParser( String expression, Map< String, Double > data ) throws Exception
+	{
+		lexer = new Lexicon( expression );
+		this.data = data;
+		parseExpression();
+	}
+	public Double getValue() throws Exception
+	{
+		while( !byteCode.isEmpty() )
+		{
+			switch( byteCode.firstElement() )
+			{
+			case "push":
+				stack.add( values.firstElement() );
+				values.remove( 0 );
+				break;
+			case "and":
+				stack.set( stack.size() - 2, Math.min( stack.get( stack.size() - 2 ), stack.get( stack.size() - 1 ) ) );
+				stack.remove( stack.size() - 1 );
+				break;
+			case "or":
+				stack.set( stack.size() - 2, stack.get( stack.size() - 2 ) +  stack.get( stack.size() - 1 ) );
+				stack.remove( stack.size() - 1 );
+				break;
+			case "negate":
+				stack.set( stack.size() - 1, -stack.get( stack.size() - 1 ) );
+				break;
+				default:
+					throw new Exception( "Unsupportd operation \""
+							+ byteCode.firstElement() + " \"" );
+			}
+			byteCode.remove( 0 );
+		}
+		if( stack.size() != 1 )
+			throw new Exception( "Interpretor getValue() error - stack size" );
+		return stack.firstElement();
+	}
+	
+	private void parseExpression() throws Exception
+	{
+		parseOr();
+	}
+	private void parseOr() throws Exception
+	{
+		parseAnd();
+		while( lexer.getToken().toLowerCase().equals( "or" ) )
+		{
+			String token = lexer.getToken().toLowerCase();
+			lexer.advance();
+			parseAnd();
+			byteCode.add( token );
+		}
+	}
+	private void parseAnd() throws Exception
+	{
+		parsePrefix();
+		while( lexer.getToken().toLowerCase().equals( "and" ) )
+		{
+			String token = lexer.getToken().toLowerCase();
+			lexer.advance();
+			parsePrefix();
+			byteCode.add( token );
+		}
+	}
+	private void parsePrefix() throws Exception
+	{
+		Vector< String > codeAppend = new Vector< String >();
+		while( lexer.getToken().equals( "-" ) )
+		{
+			codeAppend.add( "negate" );
+			lexer.advance();
+		}
+		parsePostfix();
+		for( String code : codeAppend )
+			byteCode.add( code );
+	}
+	private void parsePostfix() throws Exception
+	{
+		parseClause();
+		//no postfix necessary
+	}
+	private void parseClause() throws Exception
+	{
+		if( lexer.getToken().equals( "(" ) )
+			parseParenthesis();
+		else
+		{
+			if( data.containsKey( lexer.getToken() ) )
+				values.add( data.get( lexer.getToken() ) );
+			else
+				values.add( substitute( lexer.getToken() ) );
+			
+			byteCode.add( "push" );
+			lexer.advance();
+		}
+	}
+	private void parseParenthesis() throws Exception
+	{
+		if( !lexer.getToken().equals( "(" ) )
+			throw new Exception( "\"(\" expected" );
+		lexer.advance();
+		parseExpression();
+		if( !lexer.getToken().equals( ")" ) )
+			throw new Exception( "Parser error: missing \")\"" );
+		lexer.advance();
+	}
+
+	protected Double substitute( String token )
+	{
+		System.out.println( "Token \"" + token + "\" not in database, replacing val with zero" );
+		return 0.0;
+	}
+}
