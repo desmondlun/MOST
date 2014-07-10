05f1dbb9f18c1006b3f67fcf24ae2ba69f281662
Tony <CodeGuro@gmail.com>
2014-07-10 16:00:08 -0400

    Added javadocs for ConfigProperties.java

--- src/edu/rutgers/MOST/data/ConfigProperties.java
+++ src/edu/rutgers/MOST/data/ConfigProperties.java
@@ -17,36 +17,60 @@ public class ConfigProperties {
 	private static String quadraticSolverName;
 	private static String nonlinearSolverName;
 
+	/**
+	 * Get the mixed-integer-linear problem solver name
+	 * @return String containing the MIL solver name
+	 */
 	public static String getMixedIntegerLinearSolverName() {
 		return mixedIntegerLinearSolverName;
 	}
 
+	/**
+	 * Set the mixed-integer linear problem solver name
+	 * @param mixedIntegerLinearSolverName The String containing the solver name
+	 */
 	public static void setMixedIntegerLinearSolverName(
 			String mixedIntegerLinearSolverName) {
 		ConfigProperties.mixedIntegerLinearSolverName = mixedIntegerLinearSolverName;
 	}
 
+	/**
+	 * Get the quadratic problem solver name
+	 * @return A string containing the quadratic solver name
+	 */
 	public static String getQuadraticSolverName() {
 		return quadraticSolverName;
 	}
 
+	/**
+	 * Set the quadratic solver name
+	 * @param quadraticSolverName The String containing the solver name
+	 */
 	public static void setQuadraticSolverName(String quadraticSolverName) {
 		ConfigProperties.quadraticSolverName = quadraticSolverName;
 	}
 
+	/**
+	 * Get the Nonlinear solver name
+	 * @return A string containing the nonlinear solver name
+	 */
 	public static String getNonlinearSolverName() {
 		return nonlinearSolverName;
 	}
 
+	/**
+	 * Set the nonlinear solver name
+	 * @param nonlinearSolverName The string containing the nonlinear solver name
+	 */
 	public static void setNonlinearSolverName(String nonlinearSolverName) {
 		ConfigProperties.nonlinearSolverName = nonlinearSolverName;
 	}
 
 	/**
-	 * 
-	 * @param linearSolverName
-	 * @param quadraticSolverName
-	 * @param nonLinearSolverName
+	 * Write the configuration properties to the MOST directory
+	 * @param linearSolverName The string containing the linear solver name
+	 * @param quadraticSolverName The string containing the quadratic solver name
+	 * @param nonLinearSolverName The string containing the nonlinear solver name
 	 */
 	public static void writeToFile(String linearSolverName, String quadraticSolverName,
 			String nonLinearSolverName) {
@@ -79,6 +103,12 @@ public class ConfigProperties {
 		}
 	}
 
+	/**
+	 * Read from the file and set the configurations for each of the solvers
+	 * @see setNonlinearSolverName
+	 * @see setQuadraticSolverName
+	 * @see setMixedIntegerLinearSolverName
+	 */
 	public static void readFile() {
 		Properties prop = new Properties();
 		InputStream input = null;
@@ -116,8 +146,8 @@ public class ConfigProperties {
 	
 	/**
 	 * Create path for config.properties file
-	 * @param name
-	 * @return
+	 * @param name The string containing the file name
+	 * @return the name passed to propertiesPath
 	 */
 	private static String propertiesPath(String name) {
 		Utilities u = new Utilities();
