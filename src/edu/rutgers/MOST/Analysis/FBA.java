033d7307ae7cb8ed2feb2f338f92cc5f4a546754
Tony <CodeGuro@gmail.com>
2014-08-05 17:36:48 -0400

    FBA uses FVA by default
    Debugging

--- src/edu/rutgers/MOST/Analysis/FBA.java
+++ src/edu/rutgers/MOST/Analysis/FBA.java
@@ -1,6 +1,9 @@
 package edu.rutgers.MOST.Analysis;
 
+import java.util.ArrayList;
+
 import edu.rutgers.MOST.optimization.solvers.LinearSolver;
+import edu.rutgers.MOST.optimization.solvers.QuadraticSolver;
 import edu.rutgers.MOST.optimization.solvers.Solver;
 import edu.rutgers.MOST.optimization.solvers.SolverFactory;
 
@@ -12,6 +15,26 @@ public class FBA extends Analysis
 	{
 		super();
 	}
+	
+	@Override
+ 	public ArrayList< Double > run() throws Exception
+ 	{	
+ 		this.setSolverParameters();
+ 		this.maxObj = linearSolver.optimize();
+ 		QuadraticSolver quadraticSolver = SolverFactory.createQuadraticSolver();
+ 		ArrayList< Double > minVariability = new ArrayList< Double >();
+ 		ArrayList< Double > maxVariability = new ArrayList< Double >();
+ 	
+ 		quadraticSolver.FVA( linearSolver.getObjectiveCoefs(), this.getMaxObj(), minVariability,
+ 				maxVariability, linearSolver.getSolverComponent() );
+ 		
+ 		for( int i = 0; i < linearSolver.getSolverComponent().variableCount(); ++i )
+ 		{
+ 			if( !minVariability.get( i ).equals( maxVariability.get( i ) ) )
+ 				System.out.println( "index: " + i + "\nmin: " + minVariability.get( i ) + "\nmax: " + maxVariability.get( i ) + "\n" );
+ 		}
+ 		return linearSolver.getSoln();
+ 	}
 
 	@Override
 	public Solver getSolver()
