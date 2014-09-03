21e4349c2f7d17d30d936c1b616cd50352b94488
Jim Kelley <kelljim0414@yahoo.com>
2014-09-03 17:34:15 -0400

    Changed to Maximum Number of Knockouts

--- src/edu/rutgers/MOST/presentation/GDBBConstants.java
+++ src/edu/rutgers/MOST/presentation/GDBBConstants.java
@@ -4,7 +4,7 @@ public class GDBBConstants {
 
 	public static final String GDBB_DIALOG_TITLE = GraphicalInterfaceConstants.TITLE + " - " + "GDBB";
 	
-	public static final String NUM_KNOCKOUTS_LABEL = "   Number of Knockouts";
+	public static final String NUM_KNOCKOUTS_LABEL = "  Maximum Number of Knockouts";
 	public static final String NUM_THREADS_LABEL = "   Number of Threads";
 	public static final String SYN_OBJ_COLUMN_LABEL = "   Synthetic Objective Vector";
 	public static final String INDEFINITE_TIME_LABEL = "Indefinite Optimizer Time";
